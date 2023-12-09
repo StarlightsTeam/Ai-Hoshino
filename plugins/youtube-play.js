4df70004a16a091689cdfd90f7ed03c7e5d0e6b5
NakanoTeam <149483194+NakanoTeam@users.noreply.github.com>
2023-12-09 16:41:45 -0600

    Add files via upload

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
         "mp3doc",
         "mp4doc"
     ]
- //  let ytmsg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "17608914335-1625305606@g.us" } : {}) }, message: { "extendedTextMessage": { "text":'Downloader YT', "title": 'Ai Hoshino - MD', 'jpegThumbnail': ytlogo}}}
+    let ytmsg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "17608914335-1625305606@g.us" } : {}) }, message: { "extendedTextMessage": { "text":'Downloader YT', "title": 'Ai Hoshino - MD', 'jpegThumbnail': ytlogo}}}
     
     let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
     if (!lister.includes(feature)) return conn.reply(m.chat, `*🚩 Ingresa el formato en que deseas descargar más el titulo de un video o musica de YouTube.*\n\nEjemplo : ${usedPrefix + command} *mp3* SUICIDAL-IDOL - ecstacy\n\nFormatos disponibles :\n${usedPrefix + command} *mp3*\n${usedPrefix + command} *mp3doc*\n${usedPrefix + command} *mp4*\n${usedPrefix + command} *mp4doc*`, m, adReply)
@@ -25,16 +25,37 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
 	   txt += `│🍭 *Título ∙* ${vid.title}\n`
        txt += `│🕜 *Duración ∙* ${vid.timestamp}\n`
        txt += `│👁 *Visitas ∙* ${vid.views}\n`
+       txt += `│📚 *Autor ∙* ${vid.author.name}\n`
        txt += `│📅 *Publicado ∙* ${vid.ago}\n`
+       txt += `│⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`
        txt += `╰──────────✰`
-        
-       await conn.sendFile(m.chat, vid.thumbnail, 'out.png', txt, m)
+let buttonMessage= {
+'document': { url: `https://github.com/NakanoTeam/Ai-Hoshino` },
+'mimetype': `application/pdf`,
+'fileName': `✰ | YᴏᴜTᴜʙᴇ Pʟᴀʏ 📥`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': `${vid.url}`,
+'mediaType': 2,
+'previewType': 'VIDEO',
+'title': `${vid.title}`,
+'body': null,
+'thumbnail': await (await fetch(vid.thumbnail)).buffer(),
+'sourceUrl': 'https://youtube.com/' }},
+'caption': txt,
+'footer': '\nVideos de YouTube',
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: ytmsg })
        
        try {
        let yt = await fg.yta(vid.url, q)
        let { title, dl_url, size } = yt
        
-       await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: vid.title + '.mp3', quoted: m, contextInfo: {
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: vid.title + '.mp3', quoted: m, contextInfo: {
 'forwardingScore': 200,
 'isForwarded': true,
 externalAdReply:{
@@ -75,10 +96,31 @@ thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
 	   txt += `│🍭 *Título ∙* ${vid.title}\n`
        txt += `│🕜 *Duración ∙* ${vid.timestamp}\n`
        txt += `│👁 *Visitas ∙* ${vid.views}\n`
+       txt += `│📚 *Autor ∙* ${vid.author.name}\n`
        txt += `│📅 *Publicado ∙* ${vid.ago}\n`
+       txt += `│⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`
        txt += `╰──────────✰`
-       
-       await conn.sendFile(m.chat, vid.thumbnail, 'out.png', txt, m)
+let buttonMessage= {
+'document': { url: `https://github.com/NakanoTeam/Ai-Hoshino` },
+'mimetype': `application/pdf`,
+'fileName': `✰ | YᴏᴜTᴜʙᴇ Pʟᴀʏ 📥`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': `${vid.url}`,
+'mediaType': 2,
+'previewType': 'VIDEO',
+'title': `${vid.title}`,
+'body': null,
+'thumbnail': await (await fetch(vid.thumbnail)).buffer(),
+'sourceUrl': 'https://youtube.com/' }},
+'caption': txt,
+'footer': '\nVideos de YouTube',
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: ytmsg })
        
        let ytestilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${vid.title}`, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
        
@@ -109,10 +151,31 @@ thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
 	   txt += `│🍭 *Título ∙* ${vid.title}\n`
        txt += `│🕜 *Duración ∙* ${vid.timestamp}\n`
        txt += `│👁 *Visitas ∙* ${vid.views}\n`
+       txt += `│📚 *Autor ∙* ${vid.author.name}\n`
        txt += `│📅 *Publicado ∙* ${vid.ago}\n`
+       txt += `│⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`
        txt += `╰──────────✰`
-       
-       await conn.sendFile(m.chat, vid.thumbnail, 'out.png', txt, m)
+let buttonMessage= {
+'document': { url: `https://github.com/NakanoTeam/Ai-Hoshino` },
+'mimetype': `application/pdf`,
+'fileName': `✰ | YᴏᴜTᴜʙᴇ Pʟᴀʏ 📥`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': `${vid.url}`,
+'mediaType': 2,
+'previewType': 'VIDEO',
+'title': `${vid.title}`,
+'body': null,
+'thumbnail': await (await fetch(vid.thumbnail)).buffer(),
+'sourceUrl': 'https://youtube.com/' }},
+'caption': txt,
+'footer': '\nVideos de YouTube',
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: ytmsg })
        
        try {
        let yt = await fg.yta(vid.url, q)
@@ -159,10 +222,31 @@ thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
 	   txt += `│🍭 *Título ∙* ${vid.title}\n`
        txt += `│🕜 *Duración ∙* ${vid.timestamp}\n`
        txt += `│👁 *Visitas ∙* ${vid.views}\n`
+       txt += `│📚 *Autor ∙* ${vid.author.name}\n`
        txt += `│📅 *Publicado ∙* ${vid.ago}\n`
+       txt += `│⛓ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`
        txt += `╰──────────✰`
-       
-       await conn.sendFile(m.chat, vid.thumbnail, 'out.png', txt, m)
+let buttonMessage= {
+'document': { url: `https://github.com/NakanoTeam/Ai-Hoshino` },
+'mimetype': `application/pdf`,
+'fileName': `✰ | YᴏᴜTᴜʙᴇ Pʟᴀʏ 📥`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': `${vid.url}`,
+'mediaType': 2,
+'previewType': 'VIDEO',
+'title': `${vid.title}`,
+'body': null,
+'thumbnail': await (await fetch(vid.thumbnail)).buffer(),
+'sourceUrl': 'https://youtube.com/' }},
+'caption': txt,
+'footer': '\nVideos de YouTube',
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: ytmsg })
        
        try {
        let yt = await fg.ytv(vid.url, q)
