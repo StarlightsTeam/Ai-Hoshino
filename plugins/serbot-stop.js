23a3cc9b9e94c8b7fc7af0d511a7d294db09d694
Starlights Team <149483194+NakanoTeam@users.noreply.github.com>
2024-03-27 23:34:19 -0600

    Update serbot-stop.js

--- plugins/serbot-stop.js
+++ plugins/serbot-stop.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) => {
+/*let handler = async (m, { conn }) => {
   if (global.conn.user.jid === conn.user.jid) {
    await conn.reply(m.chat, '¿Por qué no vas directamente a la terminal?', m)
   } else {
@@ -11,4 +11,4 @@ handler.tags = ['serbot']
 handler.command = ['stop', 'stopbot', 'stopbebot']
 handler.owner = true
 
-export default handler
+export default handler*/
