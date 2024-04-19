44464bdd7850a47d9ea41f16b6a9c1106484ae76
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-04-19 17:34:31 -0600

    Add files via upload

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) return conn.reply(m.chat, `*🚩 Ingresa un texto para empezar a hablar con la Bot*`, m, adReply)
+  if (!text) return conn.reply(m.chat, `🍭 Ingresa un texto para empezar a hablar con la Bot.`, m)
   try {
     const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
     const resSimi = await api.json();
@@ -20,7 +20,6 @@ const handler = async (m, {text, command, args, usedPrefix}) => {
       const resu2 = await reis2.json();
       await conn.reply(m.chat, resu2[0][0][0], m, adReply)
     } catch {
-      return conn.reply(m.chat, `*☓ Ocurrió un error inesperado*`, m, adReply)
     }
   }
 };
