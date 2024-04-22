d0dd3ac94e4b92e2026fae6a8969eb854d96603d
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-04-22 18:12:37 -0500

    downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 try {
 if (!text) throw m.reply(`🍭 *Ingrese el link del video/imagen de pinterest*`)
-let res = await axios.get(`https://apis-starlights-team.koyeb.app/api/pindl?url=${text}`)
+let res = await axios.get(`https://api-starlights-team.koyeb.app/api/pindl?url=${text}`)
 let { type, url: sms } = res.data
 if (type === 'image') {
  await conn.sendMessage(m.chat, { image: { url: sms }, quoted: m })
