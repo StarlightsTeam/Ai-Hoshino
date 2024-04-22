bb90989570495bb7305c719ca5d05ff5b6e70f3e
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-04-22 02:57:57 -0500

    downloader-pinterest.js

---
+++ plugins/downloader-pinterest.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+try {
+if (!text) throw m.reply(`🍭 *Ingrese el link del video/imagen de pinterest*`)
+let res = await axios.get(`https://apis-starlights-team.koyeb.app/api/pindl?url=${text}`)
+let { type, url: sms } = res.data
+if (type === 'image') {
+ await conn.sendMessage(m.chat, { image: { url: sms }, quoted: m })
+} else if (type === 'video') {
+await conn.sendMessage(m.chat, { video: { url: sms }, quoted: m })
+} else {
+throw m.reply(`Error`)
+}} catch (error) {
+}}
+handler.tags = ['downloader']
+handler.command = /^(pindl)$/i
+handler.register = true 
+handler.limit = 1
+export default handler
