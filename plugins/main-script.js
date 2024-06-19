a6fd3c0e9c9ab459ed4b020f950fd0a418f4be5a
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-06-18 21:31:46 -0600

    Add files via upload

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -13,7 +13,7 @@ let txt = `*乂  S C R I P T  -  M A I N*\n\n`
     txt += `	✩   *Url* : ${json.html_url}\n`
     txt += `	✩   *Forks* : ${json.forks_count}\n`
     txt += `	✩   *Stars* : ${json.stargazers_count}\n\n`
-    txt += `🚩 > *${textbot}*`
+    txt += `> 🚩 *${textbot}*`
 let img = await (await fetch(`https://i.ibb.co/LQKxczm/file.jpg`)).buffer()
 
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
