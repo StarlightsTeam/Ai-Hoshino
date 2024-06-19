29c9cf64b899d403495aea88c6943fbea033832b
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-06-19 17:29:29 -0600

    Add files via upload

---
+++ plugins/downloader-tiktokvid.js
@@ -0,0 +1,33 @@
+import Starlights from '@StarlightsTeam/Scraper'
+
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+  if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
+  await m.react('🕓')
+  try {
+    let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text)
+
+      let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
+          txt += `    ✩  *Título* : ${title}\n`
+          txt += `    ✩  *Autor* : ${author}\n`
+          txt += `    ✩  *Duración* : ${duration} segundos\n`
+          txt += `    ✩  *Vistas* : ${views}\n`
+          txt += `    ✩  *Likes* : ${likes}\n`
+          txt += `    ✩  *Comentarios* : ${comments_count}\n`
+          txt += `    ✩  *Compartidos* : ${share_count}\n`
+          txt += `    ✩  *Publicado* : ${published}\n`
+          txt += `    ✩  *Descargas* : ${download_count}\n\n`
+          txt += `> 🚩 ${textbot}`
+
+      await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m)
+      await m.react('✅')
+
+  } catch {
+    await m.react('✖️')
+  }
+}
+handler.help = ['tiktokvid *<búsqueda>*']
+handler.tags = ['downloader']
+handler.command = ['ttvid', 'tiktokvid']
+handler.register = true
+
+export default handler
