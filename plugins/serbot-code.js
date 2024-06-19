32c8ce2f4811923279a92cfec2a2d2ec5ad3329e
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-06-19 08:49:52 -0600

    Add files via upload

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -130,7 +130,7 @@ async function connectionUpdate(update) {
     
 		await parent.reply(conn.user.jid, `La siguiente vez que se conecte envía el siguiente mensaje para iniciar sesión sin utilizar otro código `, m, rcanal)
 		
-		await parent.reply(conn.user.jid, {text : usedPrefix + command + " " + Buffer.from(fs.readFileSync("./serbot/" + authFolderB + "/creds.json"), "utf-8").toString("base64")}, m, rcanal)
+		await parent.sendMessage(conn.user.jid, {text : usedPrefix + command + " " + Buffer.from(fs.readFileSync("./serbot/" + authFolderB + "/creds.json"), "utf-8").toString("base64")}, { quoted: m })
 	  }
  
   }
