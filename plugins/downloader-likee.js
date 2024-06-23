353b3cf1b010b112a1bfe39ad635084507d3358b
@samu*trrs <samustofc330@gmail.com>
2024-06-23 16:49:57 -0500

    downloader-likee.js

---
+++ plugins/downloader-likee.js
@@ -0,0 +1,17 @@
+//Para Usuarios De Likee 🙃
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, '🚩 *Ingrese la url de un video de Likee*', m)
+try {
+let app = await fetch(`https://apis-starlights-team.koyeb.app/starlight/like-downloader?url=${text}`, { headers: { 'Content-Type': 'application/json' }})
+let json = await app.json()
+let video = json.links['no watermark']
+await conn.sendFile(m.chat, video, 'samu.mp4', `${json.caption}`, m)
+} catch (error) {
+}}
+handler.help = ['likeedl *<url>*']
+handler.tags = ['downloader']
+handler.command = /^(likeedl)$/i
+export default handler
+
