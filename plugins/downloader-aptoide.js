d32d4eb9eb326505056a9bb8be8a2a4408a53074
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-06-25 21:38:24 -0600

    Add files via upload

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -22,5 +22,5 @@ handler.help = ['aptoide *<búsqueda>*']
 handler.tags = ['downloader']
 handler.command = ['aptoide', 'apk']
 handler.register = true 
-handler.limit = 5
+//handler.limit = 5
 export default handler
