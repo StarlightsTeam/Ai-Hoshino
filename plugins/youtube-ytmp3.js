d6970af8d23f7144112abdbc8f9ba6bb423cf158
Starlights Team <149483194+StarlightsTeam@users.noreply.github.com>
2024-07-21 03:06:28 -0600

    Add files via upload

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -48,7 +48,7 @@ if (bytes === 0) return '0 Bytes'
 let k = 1024
 let dm = decimals < 0 ? 0 : decimals
 let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-ket i = Math.floor(Math.log(bytes) / Math.log(k))
+let i = Math.floor(Math.log(bytes) / Math.log(k))
 return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
 
