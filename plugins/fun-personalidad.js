6d8a2407d389133300b2e1e972bf960f3486b4af
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-08-19 04:48:57 -0600

    Add files via upload

--- plugins/fun-personalidad.js
+++ plugins/fun-personalidad.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command, text }) => {
 	
-    if (!text) return m.reply(`🚩 Ingresa un nombre justo al comando.)
+    if (!text) return m.reply(`🚩 Ingresa un nombre justo al comando.`)
 	
   let personalidad = `
 ┏━━°❀❬ *PERSONALIDAD}* ❭❀°━━┓
--- plugins/group-invite.js
+++ plugins/group-invite.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `🚩 Ingrese el número al que quiere enviar una invitación al grupo.\n\n*Ejemplo:*\n*${usedPrefix + command}* 5218261275256`, m, rcanal)
 if (text.includes('+')) return conn.reply(`🚩 Ingrese el número todo junto sin el *+*`, m, rcanal)
-if (isNaN(text)) return conn.reply(m.chat, `*Ingrese sólo números más su código de país sin espacios*'
+if (isNaN(text)) return conn.reply(m.chat, `*Ingrese sólo números más su código de país sin espacios*`, m, rcanal)
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
  
