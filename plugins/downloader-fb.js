9e28f7547d919a0d5bef1e6629af974eca6aa01a
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-08-24 15:52:21 -0600

    Add files via upload

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -4,8 +4,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args || !args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace del vídeo de Facebook junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.facebook.com/official.trash.gang/videos/873759786348039/?mibextid=rS40aB7S9Ucbxw6v`, m, rcanal)
 await m.react('🕓')
 try {
-let { title, SD, HD } = await Starlights.fbdl(args[0])
-await conn.sendFile(m.chat, SD || HD, 'fbdl.mp4', `*» Título* : ${title}`, m, null, rcanal)
+let { dl_url } = await Starlights.fbdl(args[0])
+await conn.sendFile(m.chat, dl_url, 'fbdl.mp4', listo, m, null, rcanal)
 await m.react('✅')
 } catch {
 await m.react('✖️')
