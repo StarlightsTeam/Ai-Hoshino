6b5baad57ef06996dc2cd726fd8bfee2fa9ec118
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-08-24 19:43:27 -0600

    Add files via upload

--- plugins/tools-ai.js
+++ plugins/tools-ai.js
@@ -1,13 +1,22 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `*🚩 Ingrese su petición*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
-await m.react('💬')
-try {
-let { msg } = await Starlights.openAi(text)
-await conn.reply(m.chat, msg, m, rcanal)
-} catch {
-}}
+    if (!text) return conn.reply(m.chat, `*🚩 Ingrese su petición*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
+    await m.react('💬')
+
+    try {
+        let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/chatgpt?text=${text}`)
+        let json = await api.json()
+
+        if (json.status === 'true') {
+            await conn.reply(m.chat, json.result, m, rcanal)
+        } else {
+            await m.react('✖️')
+        }
+    } catch {
+        await m.react('✖️')
+    }
+}
 handler.help = ['ai *<petición>*']
 handler.tags = ['tools']
 handler.command = /^(miku|ai|ia|chatgpt|gpt)$/i
