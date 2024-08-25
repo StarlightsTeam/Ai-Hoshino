be0fd4e53ea5edf9ee31ff78fe4ae7b470bc0979
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-08-24 20:08:12 -0600

    Add files via upload

---
+++ plugins/tools-tweetstalk.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return conn.reply(m.chat, '🚩 Ingrese el nombre de usuario de Twitter.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Fernanfloo`, m, rcanal)
+    await m.react('💬')
+
+    try {
+        let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/tweeter-stalk?text=${text}`)
+        let json = await api.json()
+
+        if (json.user) {
+            let txt = '`乂  T W I T T E R  -  S T A L K`\n\n'
+                txt += `  ✩   *Usuario* : ${json.names}\n`
+                txt += `  ✩   *Bio* : ${json.bio}\n`
+                txt += `  ✩   *Url* : ${json.link}\n\n`
+                txt += `> 🚩 *${textbot}*`
+                
+            await conn.reply(m.chat, txt, m, rcanal)
+        } else {
+            await m.react('✖️')
+        }
+    } catch {
+        await m.react('✖️')
+    }
+}
+handler.help = ['tweestalk *<usuario>*']
+handler.tags = ['tools']
+handler.command = ['tweestalk', 'tweeter-stalk', 'twitter-stalk']
+handler.register = true
+
+export default handler
