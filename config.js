d91af568b39df6bd570d1379a9ff7e601e80df25
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-08-24 20:54:36 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/CS0G9jonrATESoRSRN3Riu'
+global.group = 'https://chat.whatsapp.com/JMZPEvW2llwBQA3APYSteb'
 global.canal = 'https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
