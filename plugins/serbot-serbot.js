4b88a9f3e9ad95955561c537a65e8d8d70205941
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-09-05 13:31:42 -0600

    Add files via upload

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -19,7 +19,7 @@ return m.reply("Este comando solo puede ser usado en el bot principal! wa.me/" +
 }
 
 async function serbot() {
-    let serbotFolder = crypto.randomBytes(10).toString('hex').slice(0, 8)
+    let serbotFolder = m.sender.split('@')[0]
     let folderSub = `./serbot/${serbotFolder}`
     if (!fs.existsSync(folderSub)) {
       fs.mkdirSync(folderSub, { recursive: true })
