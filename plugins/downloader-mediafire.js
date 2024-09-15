2a4132a6b728648d96c39d93874fa6ac6af888d7
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-09-15 17:14:13 -0300

    .

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -17,8 +17,22 @@ await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
 await m.react('✅')
 } catch {
+try {
+let { title, ext, aploud, size, dl_url } = await Starlights.mediafireV2(args[0])
+let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
+    txt += `	✩  *Nombre* : ${title}\n`
+    txt += `	✩  *Peso* : ${size}\n`
+    txt += `	✩  *Publicado* : ${aploud}\n`
+    txt += `	✩  *MimeType* : ${ext}\n\n`
+    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
+let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
+await m.react('✅')
+} catch {
 await m.react('✖️')
-}}
+}}}
+
 handler.help = ['mediafire'].map(v => v + ' *<url>*')
 handler.tags = ['downloader', 'premium']
 handler.command = ['mediafire', 'mdfire', 'mf']
