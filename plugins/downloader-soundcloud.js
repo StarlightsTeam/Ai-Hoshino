7786e653cb1aaec4518ceda7c853cab1e7205112
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-09-15 21:44:38 -0300

    .

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -1,3 +1,61 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+
+let handler = async (m, { conn, command, args, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, `🚩 Ingrese el nombre de la cancion de *Soundcloud.*`, m, rcanal)
+
+await m.react('🕒');
+try {
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/soundcloud-search?text=${encodeURIComponent(text)}`);
+let json = await api.json();
+let { url } = json[0];
+
+let api2 = await fetch(`https://apis-starlights-team.koyeb.app/starlight/soundcloud?url=${url}`);
+let json2 = await api2.json();
+        
+let { link: dl_url, quality, image } = json2;
+
+let audio = await getBuffer(dl_url);
+
+let txt = `*\`- S O U N C L O U D - M U S I C -\`*\n\n`;
+    txt += `*Título* : ${json[0].title}\n`;
+    txt += `*Calidad* : ${quality}\n`;
+    txt += `*Url* : ${url}\n\n`;
+    txt += `> 🚩 *${text}*`;
+
+await conn.sendFile(m.chat, image, 'thumbnail.jpg', txt, m);
+await conn.sendMessage(m.chat, { audio: audio, fileName: `${json[0].title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
+
+await m.react('✅');
+} catch {
+await m.react('✖️');
+}}
+
+handler.help = ['soundcloud *<búsqueda>*']
+handler.tags = ['downloader']
+handler.command = ['soundcloud', 'sound']
+
+export default handler
+
+const getBuffer = async (url, options) => {
+try {
+const res = await axios({
+method: 'get',
+url,
+headers: {
+'DNT': 1,
+'Upgrade-Insecure-Request': 1,
+},
+...options,
+responseType: 'arraybuffer',
+});
+return res.data;
+} catch (e) {
+console.log(`Error : ${e}`);
+}
+};
+
+/*
 //Instalar la dependencia Node-id3 🙃
 //Use math por problemas de que algunos audios no se envian
 //La segunda url si descarga los datos de la cancion para eso tienes que ingresar a Souncloud la musica que quieres descargar ingresas y copias el link y lo pegas en la segunda url :) 
@@ -42,4 +100,4 @@ handler.tags = ['downloader']
 handler.command = ['soundcloud', 'sound']
 handler.register = true
 //handler.limit = 3
-export default handler
+export default handler*/
