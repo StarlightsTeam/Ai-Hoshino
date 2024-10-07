410dd12a8ebf69f488358a3cbf2854667257855f
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-10-06 23:22:11 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -173,7 +173,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp13 = 'https://telegra.ph/file/ba7c4a3eb7bf3d892b0c8.mp4'
     let pp14 = 'https://tinyurl.com/ymlqb6ml'
     let pp15 = 'https://tinyurl.com/ykv7g4zy'
-    let img = await (await fetch(`https://i.ibb.co/gDjssZH/file.jpg`)).buffer()
+    let img = `./storage/img/menu.jpg`
     await m.react('⭐')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
