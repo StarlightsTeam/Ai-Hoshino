31abee5f20b8cddc86c7108424122c631d217c6c
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-10-19 16:33:04 -0600

    Add files via upload

--- plugins/youtube-play2.js
+++ plugins/youtube-play2.js
@@ -29,7 +29,16 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
   await conn.sendFile(m.chat, vid.thumbnail, 'thumbnail.jpg', txt, m, null, rcanal)
 
   try {
-    let data = feature.includes('mp3') ? await Starlights.ytmp3(vid.url) : await Starlights.ytmp4(vid.url)
+    let data = feature.includes('mp3') ? await Starlights.ytmp3v2(vid.url) : await Starlights.ytmp4v2(vid.url)
+    let isDoc = feature.includes('doc')
+    let mimetype = feature.includes('mp3') ? 'audio/mpeg' : 'video/mp4'
+    let file = { url: data.dl_url }
+
+    await conn.sendMessage(m.chat, { [isDoc ? 'document' : feature.includes('mp3') ? 'audio' : 'video']: file, mimetype, fileName: `${data.title}.${feature.includes('mp3') ? 'mp3' : 'mp4'}` }, { quoted: m })
+    await m.react('✅')
+  } catch {
+  try {
+  let data = feature.includes('mp3') ? await Starlights.ytmp3(vid.url) : await Starlights.ytmp4(vid.url)
     let isDoc = feature.includes('doc')
     let mimetype = feature.includes('mp3') ? 'audio/mpeg' : 'video/mp4'
     let file = { url: data.dl_url }
@@ -39,8 +48,8 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
   } catch {
     await m.react('✖️')
   }
+ }
 }
-
 handler.help = ['play2 <formato> <búsqueda>']
 handler.tags = ['downloader']
 handler.command = ['ytplay', 'play2']
