01ba57750d7f38da33716a58c72797bfa07db9a8
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-12-17 07:20:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -295,10 +295,10 @@ if (connection === 'close') {
 process.on('uncaughtException', console.error)
 
 let isInit = true;
-let handler = await import('./system/handler.js')
+let handler = await import('./handler.js')
 global.reloadHandler = async function(restatConn) {
   try {
-    const Handler = await import(`./system/handler.js?update=${Date.now()}`).catch(console.error);
+    const Handler = await import(`./handler.js?update=${Date.now()}`).catch(console.error);
     if (Object.keys(Handler || {}).length) handler = Handler
   } catch (e) {
     console.error(e);
