1241b236984d11dc60131d21c140ab3fbd8fc536
Irokz Dal ダーク <164250407+IrokzDal@users.noreply.github.com>
2025-02-25 20:16:07 -0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おDanịel.xyz⁩;;\nFN:おDanịel.xyz⁩\nORG:おDanịel.xyz⁩\nTITLE:\nitem1.TEL;waid=5218261000681:5218261000681\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Irokz Dal ダーク;;\nFN:Irokz Dal ダーク\nORG:Irokz Dal ダーク\nTITLE:\nitem1.TEL;waid=639705083935:639705083935\nitem1.X-ABLabel:Irokz Dal ダーク⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Irokz Dal ダーク\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Irokz Dal ダーク⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
