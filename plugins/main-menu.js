dba26d8c30383c91a16f8136ac07ac6358fb37b9
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-05-03 10:10:04 -0300

    𝐒𝐭𝐚𝐫𝐥𝐢𝐠𝐡𝐭𝐬-𝐓𝐞𝐚𝐦

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,6 +1,5 @@
 import { promises } from 'fs'
 import { join } from 'path'
-import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
 let tags = {
@@ -37,7 +36,7 @@ const defaultMenu = {
 ╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩
 ┊🍬 [ *Modo* :: *Público*
 ┊📚 [ *Baileys* :: *Multi Device*
-┊⏱ [ *Tiempo Activo* :: *%muptime*
+┊⏱ [ *Tiempo Activo* :: *%uptime*
 ┊👤 [ *Usuarios* :: *%totalreg*
 ╰─────────
 %readmore
@@ -55,36 +54,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let { exp, limit, level } = global.db.data.users[m.sender]
     let { min, xp, max } = xpRange(level, global.multiplier)
     let name = await conn.getName(m.sender)
-    let d = new Date(new Date + 3600000)
-    let locale = 'es'
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
     let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
@@ -127,7 +98,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
     let replace = {
       '%': '%',
-      p: _p, uptime, muptime,
+      p: _p, uptime, _uptime,
       taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
       wasp: '@0',
       me: conn.getName(conn.user.jid),
@@ -142,7 +113,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       totalexp: exp,
       xp4levelup: max - exp,
       github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-      greeting, level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
+      greeting, level, limit, name, totalreg, rtotalreg,
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
@@ -168,16 +139,17 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
-  } catch (e) {
-    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
-    throw e
-  }
+} catch (e) {
+await m.react('✖️')
+throw e
+}
 }
 
 handler.help = ['menu']
 handler.tags = ['main']
 handler.command = ['menu', 'help', 'menú'] 
 handler.register = true 
+
 export default handler
 
 
@@ -185,10 +157,11 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+let horas = Math.floor(ms / 3600000)
+let minutos = Math.floor(ms / 60000) % 60
+let segundos = Math.floor(ms / 1000) % 60
+  console.log({ ms, horas, minutos, segundos })
+return [horas, minutos, segundos].map((v) => v.toString().padStart(2, 0)).join(":")
 }
 
   var ase = new Date();
