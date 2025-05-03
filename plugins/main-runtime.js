1bab2735106a638ec04733e465a1144a7bde5eab
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-05-03 10:38:47 -0300

    𝐒𝐭𝐚𝐫𝐥𝐢𝐠𝐡𝐭𝐬-𝐓𝐞𝐚𝐦

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,25 +1,22 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
 	
-	let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-   m.reply(`*» Bot activo durante* : ${muptime}`) 
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+  
+m.reply(`*» Bot activo durante* : ${uptime}`) 
+	
 }
+
 handler.help = ['runtime']
 handler.tags = ['main']
 handler.command = ['runtime', 'uptime']
+
 export default handler
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+  const d = Math.floor(ms / 86400000)
+  const h = Math.floor(ms / 3600000) % 24
+  const m = Math.floor(ms / 60000) % 60
+  const s = Math.floor(ms / 1000) % 60
+  return `${d}d ${h}h ${m}m ${s}s`
 }
